62396e7a4c6f128b5e9ffd41255778156a0b43d2
Rahmat Sayfuddin <rahmatsayfuddin@gmail.com>
2020-12-02 13:39:54 +0700

    sql

--- dashboard.sql
+++ dashboard.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 01, 2020 at 03:37 AM
+-- Generation Time: Dec 02, 2020 at 07:38 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.4
 
@@ -35,18 +35,36 @@ CREATE TABLE `data` (
   `berat_badan` float DEFAULT NULL,
   `lingkar_kepala` float DEFAULT NULL,
   `lingkar_lengan_atas` float DEFAULT NULL,
-  `lingkar_perut` float DEFAULT NULL
+  `lingkar_perut` float DEFAULT NULL,
+  `hepatitis_b` int(11) DEFAULT NULL,
+  `polio` int(11) DEFAULT NULL,
+  `bcg` int(11) DEFAULT NULL,
+  `dtp` int(11) DEFAULT NULL,
+  `hib` int(11) DEFAULT NULL,
+  `pcv` int(11) DEFAULT NULL,
+  `rotavirus` int(11) DEFAULT NULL,
+  `influenza` int(11) DEFAULT NULL,
+  `campak` int(11) DEFAULT NULL,
+  `mmr` int(11) DEFAULT NULL,
+  `tifoid` int(11) DEFAULT NULL,
+  `hepatitis_a` int(11) DEFAULT NULL,
+  `varisela` int(11) DEFAULT NULL,
+  `hpv` int(11) DEFAULT NULL,
+  `japanese_encephalitis` int(11) DEFAULT NULL,
+  `dangue` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `data`
 --
 
-INSERT INTO `data` (`id`, `nama_anak`, `alamat`, `tinggi_badan`, `berat_badan`, `lingkar_kepala`, `lingkar_lengan_atas`, `lingkar_perut`) VALUES
-(1, 'baron', 'jgc', 2, 2, 2, 2, 2),
-(3, 'faf', 'fafa', 2, 2, 2, 2, 2),
-(4, 'jhc', 'dada', NULL, 43, 4, 44, 5),
-(7, 'rah', 'adad', NULL, 43, 4, 44, 5);
+INSERT INTO `data` (`id`, `nama_anak`, `alamat`, `tinggi_badan`, `berat_badan`, `lingkar_kepala`, `lingkar_lengan_atas`, `lingkar_perut`, `hepatitis_b`, `polio`, `bcg`, `dtp`, `hib`, `pcv`, `rotavirus`, `influenza`, `campak`, `mmr`, `tifoid`, `hepatitis_a`, `varisela`, `hpv`, `japanese_encephalitis`, `dangue`) VALUES
+(1, 'baron', 'jgc', 2, 2, 2, 2, 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(3, 'faf', 'fafa', 2, 2, 2, 2, 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(4, 'jhc', 'dada', 43, 43, 4, 44, 5, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(7, 'rah', 'adad', NULL, 43, 4, 44, 5, NULL, 1, 1, NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(8, 'imu', 'faug', NULL, 777, 77, 77, 77, 1, 1, 1, NULL, 1, 1, NULL, 1, 1, NULL, NULL, 1, 1, 1, NULL, 1),
+(9, 'baron', 'faf', 3, 3, 3, 3, 3, 1, 1, NULL, 1, NULL, NULL, NULL, NULL, NULL, 1, 1, 1, NULL, NULL, 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -94,7 +112,7 @@ CREATE TABLE `sys_users` (
 --
 
 INSERT INTO `sys_users` (`id`, `username`, `email`, `phone`, `status`, `profile_picture`, `password`, `last_login`, `last_logout`, `last_accessed_ip`, `logins`, `created_by`, `created_at`, `updated_by`, `updated_at`) VALUES
-(13, 'test', 'test@gmail.com', '12345678', 1, '/uploads/images/2020/May/24/1590353371_43e4efcdd22419ec37b0.png', '$2y$10$MJgRd.iID3Q1Q1HMUjXMi./Q0uearXWRxbgNofh5vono745MEKbfC', '2020-11-30 23:01:52', '2020-12-01 02:19:47', '::1', 7, 1, '2020-05-24 15:49:31', NULL, '2020-12-01 02:19:47'),
+(13, 'test', 'test@gmail.com', '12345678', 1, '/uploads/images/2020/May/24/1590353371_43e4efcdd22419ec37b0.png', '$2y$10$MJgRd.iID3Q1Q1HMUjXMi./Q0uearXWRxbgNofh5vono745MEKbfC', '2020-12-02 10:23:20', '2020-12-01 02:19:47', '::1', 9, 1, '2020-05-24 15:49:31', NULL, '2020-12-02 10:23:20'),
 (23, 'rahmatsayfuddin', 'rdlsecurity2@mailinator.com', '08888888', 1, NULL, '$2y$10$DRDJWoifKfdMOIS3SVCY4OznERiWiwt57uNr/kBc9rgKz1qxquAIi', NULL, NULL, '0.0.0.0', 0, 1, '2020-12-01 00:53:07', NULL, '2020-12-01 00:53:07');
 
 --
@@ -127,7 +145,7 @@ ALTER TABLE `sys_users`
 -- AUTO_INCREMENT for table `data`
 --
 ALTER TABLE `data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `sys_status`
